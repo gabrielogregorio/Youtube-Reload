58d1dfbc541d6572f3b42f817dd53c0e8de3a6ea
Gabriel Gregório <gabriel.gregorio.1@outlook.com>
2023-09-09 14:53:33 -0300

    refactor: upgrade navitems

--- src/components/layouts/index.tsx
+++ src/components/layouts/index.tsx
@@ -1,6 +1,5 @@
 import { Navbar } from '@/layouts/navbar';
 import type { ReactElement } from 'react';
-import { useCurrentScreen } from '@/hooks/useCurrentScreen';
 import { Notify } from '@/layouts/Notify';
 import { Profile } from '@/layouts/Profile';
 
@@ -9,17 +8,14 @@ interface ITemplateDefaultProps {
 }
 
 export const MainLayout = ({ children }: ITemplateDefaultProps) => {
-  const { currentScreen } = useCurrentScreen();
-
   return (
     <div className="relative mt-[120px]">
       <header className="fixed top-0 left-0 z-50 w-full flex items-center justify-end bg-dark/50 backdrop-blur-sm shadow-md h-14 ">
         <div className="ml-6 hidden md:block" />
         <div className="ml-2 w-14 h-full hidden md:block" />
-
         <div className="ml-2 w-14 h-full hidden md:block" />
 
-        <Navbar activeScreen={currentScreen} />
+        <Navbar />
 
         <Notify />
 
--- src/components/layouts/navbar/navbarItem.tsx
+++ src/components/layouts/navbar/navbarItem.tsx
@@ -1,5 +1,6 @@
 import type { ScreenEnum } from '@/contracts/homeScreens';
 import { useCurrentScreen } from '@/hooks/useCurrentScreen';
+import { tailwindMerge } from '@/libs/tailwindMerge';
 import { LogService } from '@/services/log/LogService';
 import type { ReactNode } from 'react';
 
@@ -11,17 +12,24 @@ interface INavbarItemProps {
 }
 
 export const NavbarItem = ({ text, nameScreen, isActive, icon }: INavbarItemProps) => {
-  const styleOnActive = isActive ? 'border-red-light text-red-light' : 'border-transparent text-white';
   const { updateCurrentScreen } = useCurrentScreen();
+
+  const handleClickNavItem = () => {
+    LogService.addBreadcrumb({ type: 'click', level: 'info', message: `update screen to ${nameScreen}` });
+    updateCurrentScreen(nameScreen);
+  };
+
+  const styleOnActive = isActive ? 'border-red-light text-red-light' : 'border-transparent text-white';
 
   return (
     <button
       type="button"
-      onClick={() => {
-        LogService.addBreadcrumb({ type: 'click', level: 'info', message: `update screen to ${nameScreen}` });
-        updateCurrentScreen(nameScreen);
-      }}
-      className={`border-b-[3px] bg-transparent decoration-none p-[20px] pb-[5px] mb-[15px] cursor-pointer transition-all duration-150 hover:scale-110 ${styleOnActive} select-none font-bold flex items-center justify-center`}>
+      onClick={handleClickNavItem}
+      className={tailwindMerge(
+        'border-b-[3px] bg-transparent decoration-none p-[20px] pb-[5px] mb-[15px] cursor-pointer transition-all duration-150 hover:scale-110 select-none font-bold flex items-center justify-center',
+        styleOnActive,
+      )}
+      aria-label={`Navigate to ${text}`}>
       <span className="md:hidden">{icon}</span>
       <span className="ml-2 hidden md:block text-[0.9rem]">{text}</span>
     </button>
