1544ac64e5bed14c0954ec16476256720d775a35
Gabriel Gregório <gabriel.gregorio.1@outlook.com>
2023-09-08 22:22:20 -0300

    fix: version on deploy

--- src/pages/base.tsx
+++ src/pages/base.tsx
@@ -27,7 +27,7 @@ export const BaseScreens = (): ReactElement => {
   return (
     <TemplateDefault activeScreen={screenSelected}>
       <>
-        {envs.VITE_ENVIRONMENT ? (
+        {envs.VITE_ENVIRONMENT && envs.VITE_ENVIRONMENT !== 'production' ? (
           <div className="text-[2rem] font-[800] fixed top-1 left-4 z-[190] text-white/75">{envs.VITE_ENVIRONMENT}</div>
         ) : undefined}
         <Header />
